304ec81cc556226b2e0485727260d25d0a8253e2
minisemi <a.ulander@live.se>
2016-06-23 16:43:34 +0200

    layouts

---
+++ app/src/main/java/a46elks/a46elksapp/groups/GroupsActivity.java
@@ -0,0 +1,17 @@
+package a46elks.a46elksapp.groups;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import a46elks.a46elksapp.R;
+
+public class GroupsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_groups);
+    }
+
+
+}
