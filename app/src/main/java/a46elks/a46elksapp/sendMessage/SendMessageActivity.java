81118c49fc82a012f55c03d57396bd5edda86b14
minisemi <a.ulander@live.se>
2016-06-28 10:58:18 +0200

    UI

---
+++ app/src/main/java/a46elks/a46elksapp/sendMessage/SendMessageActivity.java
@@ -0,0 +1,15 @@
+package a46elks.a46elksapp.sendMessage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import a46elks.a46elksapp.R;
+
+public class SendMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_message);
+    }
+}
