87abd7850869967b3eb346580efeb63bf1521794
minisemi <a.ulander@live.se>
2016-07-22 18:13:13 +0200

    signup fixed and accouynt creation started

---
+++ app/src/main/java/a46elks/a46elksapp/SessionManager.java
@@ -0,0 +1,114 @@
+package a46elks.a46elksapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import java.util.HashMap;
+
+/**
+ * Created by Alexander on 2016-07-22.
+ */
+public class SessionManager {
+
+    SharedPreferences pref;
+
+    // Editor for Shared preferences
+    SharedPreferences.Editor editor;
+
+    // Context
+    Context context;
+
+    // Shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Sharedpref file name
+    private static final String PREF_NAME = "a46elksapp_sharedpref";
+
+    // All Shared Preferences Keys
+    private static final String IS_LOGIN = "IsLoggedIn";
+
+    // User name (make variable public to access from outside)
+    public static final String KEY_API_USERNAME = "id";
+
+    // Email address (make variable public to access from outside)
+    public static final String KEY_API_PASSWORD = "secret";
+
+    // Constructor
+    public SessionManager(Context context){
+        this.context = context;
+        pref = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void createLoginSession (String name, String email){
+        // Storing login value as TRUE
+        editor.putBoolean(IS_LOGIN, true);
+
+        // Storing name in pref
+        editor.putString(KEY_API_USERNAME, name);
+
+        // Storing email in pref
+        editor.putString(KEY_API_PASSWORD, email);
+
+        // commit changes
+        editor.commit();
+    }
+
+    public HashMap<String, String> getUserDetails(){
+        HashMap<String, String> user = new HashMap<String, String>();
+        // user name
+        user.put(KEY_API_USERNAME, pref.getString(KEY_API_USERNAME, null));
+
+        // user email id
+        user.put(KEY_API_PASSWORD, pref.getString(KEY_API_PASSWORD, null));
+
+        // return user
+        return user;
+    }
+
+    public void checkLogin(){
+        // Check login status
+        if(!this.isLoggedIn()){
+            // user is not logged in redirect him to Login Activity
+            Intent i = new Intent(context, LoginActivity.class);
+            // Closing all the Activities
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            // Add new Flag to start new Activity
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            // Staring Login Activity
+            context.startActivity(i);
+        }
+
+    }
+
+    /**
+     * Quick check for login
+     * **/
+    // Get Login State
+    public boolean isLoggedIn(){
+        return pref.getBoolean(IS_LOGIN, false);
+    }
+
+    /**
+     * Clear session details
+     * */
+    public void logoutUser(){
+        // Clearing all data from Shared Preferences
+        editor.clear();
+        editor.commit();
+
+        // After logout redirect user to Loing Activity
+        Intent i = new Intent(context, LoginActivity.class);
+        // Closing all the Activities
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        // Add new Flag to start new Activity
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        // Staring Login Activity
+        context.startActivity(i);
+    }
+}
